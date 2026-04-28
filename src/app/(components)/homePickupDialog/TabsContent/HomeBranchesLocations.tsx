223ba73faddacc3c3e41e9725353beb090519af2
mohamedAshraf18102000 <mohamed.ashraf@viganium.com>
2026-04-28 13:49:08 +0300

    feat: integrate user address retrieval and selection in HomeBranchesLocations component for enhanced user experience

--- src/app/(components)/homePickupDialog/TabsContent/HomeBranchesLocations.tsx
+++ src/app/(components)/homePickupDialog/TabsContent/HomeBranchesLocations.tsx
@@ -1,13 +1,42 @@
 "use client";
+import { useEffect, useState } from "react";
 import GoogleMapsLocation from "../../mapsLocation/GoogleMapsLocation";
 import { useBookedCarDetailsStore } from "@/lib/stores/useBookedCarDetailsStore";
 import { useUserPreferedFiltersStore } from "@/lib/stores/useUserPreferedFiltersStore";
 import { usePickupDialogStore } from "@/lib/stores/usePickupDialogStore";
+import { UserSavedAddresses } from "@/app/[locale]/(dialogs)/PickupDialog/UserSavedAddresses";
+import { getUserAddress } from "@/services/userProfile/getUserAddress.service";
+import { UserAddress } from "@/types/userProfile/userAddress";
 
 const HomeBranchesLocations = () => {
   const { setFormData } = useBookedCarDetailsStore();
   const { setFilter } = useUserPreferedFiltersStore();
   const { setIsUnsavedMapLocation } = usePickupDialogStore();
+  const [userAddresses, setUserAddresses] = useState<UserAddress[]>([]);
+  const [tempLocation, setTempLocationState] = useState<{
+    lat: number;
+    lng: number;
+    address: string;
+  } | null>(null);
+
+  useEffect(() => {
+    let isMounted = true;
+    getUserAddress()
+      .then((addresses) => {
+        if (isMounted) {
+          setUserAddresses(addresses ?? []);
+        }
+      })
+      .catch(() => {
+        if (isMounted) {
+          setUserAddresses([]);
+        }
+      });
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
 
   const handleBranchMapSelection = (
     lat: number,
@@ -37,13 +66,25 @@ const HomeBranchesLocations = () => {
     setIsUnsavedMapLocation(false);
   };
 
+  const setTempLocation = (location: { lat: number; lng: number; address: string }) => {
+    setTempLocationState(location);
+    handleBranchMapSelection(location.lat, location.lng, location.address);
+  };
+
   return (
-    <GoogleMapsLocation
-      storeless
-      onLocationChange={(lat, lng, address) =>
-        handleBranchMapSelection(lat, lng, address)
-      }
-    />
+    <div className="relative">
+      <GoogleMapsLocation
+        storeless
+        selectedLat={tempLocation?.lat}
+        selectedLng={tempLocation?.lng}
+        onLocationChange={(lat, lng, address) => setTempLocation({ lat, lng, address })}
+      />
+      <UserSavedAddresses
+        userAddresses={userAddresses}
+        tempLocation={tempLocation}
+        setTempLocation={setTempLocation}
+      />
+    </div>
   );
 };
 
