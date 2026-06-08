759964f277aeb1814424008cbc681efdb5fac596
mohamedAshraf18102000 <your-email@example.com>
2026-06-08 11:15:52 +0300

    refactor: replace maintenance page content with a dedicated MaintenanceComponent for improved structure and readability

--- src/app/[locale]/maintenance/page.tsx
+++ src/app/[locale]/maintenance/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from "@/i18n/routing";
 import { getHomePageDetails } from "@/services/home/home.service";
 import { setRequestLocale } from "next-intl/server";
-import Link from "next/link";
+import { MaintenanceComponent } from "./MaintenanceComponent";
 
 export const dynamic = "force-dynamic";
 
@@ -26,14 +26,7 @@ const MaintenancePage = async ({ params }: Props) => {
     redirect({ href: "/", locale });
   }
 
-  return (
-    <div className="min-h-screen w-full p-3 bg-red-950 text-white flex items-center justify-center text-4xl sm:text-6xl md:text-xl font-bold text-center">
-      تحت التطوير
-      <div>
-        <Link href="/">CLICK HERE </Link>
-      </div>
-    </div>
-  );
+  return <MaintenanceComponent />;
 };
 
 export default MaintenancePage;
